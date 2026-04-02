87a907e72f5e02ee1e43991ee68311735ca2602d
Mohammed Serrou <125736644+smixjp2@users.noreply.github.com>
2026-04-02 14:58:54 +0100

    QSQ

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,6 @@
-// Middleware eliminado
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next()
+}
